62149dca0ee3ac7dbd0551226e09ec7248ca4b41
horankev <horank@gmail.com>
2023-11-26 19:53:50 +0000

    fix .data[[]] issue

--- R/make_cont_k_islands.R
+++ R/make_cont_k_islands.R
@@ -5,14 +5,14 @@ make_cont_k_islands <- function(data, # sf dataframe
 
   unit1 <- deparse(substitute(unit))
 
-  data1 <- data |> dplyr::group_by(unit1) |> dplyr::summarise()
+  data1 <- data %>% dplyr::group_by(.data[[unit1]]) %>% dplyr::summarise()
 
   # link_islands_k
 
   if (link_islands_k > 0)
   {
     # unconnected units
-    cont <-  data1 |>
+    cont <-  data1 %>%
       sf::st_intersects()
     still_unconnected <- lengths(lapply(cont, function(lst) lst))
     unconnected <- which(still_unconnected == 1)
@@ -23,8 +23,8 @@ make_cont_k_islands <- function(data, # sf dataframe
     )
     for (i in 1:length(unconnected))
     {
-      distances <- sf::st_distance(data1$geometry[unconnected[i]], sf::st_geometry(data1$geometry)) |>
-        as.numeric() |> sort()
+      distances <- sf::st_distance(data1$geometry[unconnected[i]], sf::st_geometry(data1$geometry)) %>%
+        as.numeric() %>% sort()
       distdf[i,2] <- round(distances[link_islands_k+1]) +100
       distdf[i,1] <- unconnected[i]
     }
@@ -34,12 +34,12 @@ make_cont_k_islands <- function(data, # sf dataframe
       bufs[distdf[i,1]] <- distdf[i,2]
     }
 
-    cont <- data1 |>
-      sf::st_buffer(dist=bufs) |>
-      sf::st_intersects() |>
+    cont <- data1 %>%
+      sf::st_buffer(dist=bufs) %>%
+      sf::st_intersects() %>%
       purrr::imap(~setdiff(.x,.y))
 
-    names(cont) <- data1 |> dplyr::pull(unit1)
+    names(cont) <- data1 %>% dplyr::pull(unit1)
     class(cont) <- "nb"
 
     return(cont)
@@ -49,11 +49,11 @@ make_cont_k_islands <- function(data, # sf dataframe
   # otherwise, just return unaltered contiguity structure
   if(link_islands_k <= 0)
   {
-    cont <- data1 |>
-      sf::st_intersects() |>
+    cont <- data1 %>%
+      sf::st_intersects() %>%
       purrr::imap(~setdiff(.x,.y))
 
-    names(cont) <- data1 |> dplyr::pull(unit1)
+    names(cont) <- data1 %>% dplyr::pull(unit1)
     class(cont) <- "nb"
   }
 
