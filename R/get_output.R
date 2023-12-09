84b8ef9b844a1310ba036c2ec940fbbfc5abf48f
horankev <horank@gmail.com>
2023-12-09 19:11:46 +0000

    add dependencies

--- R/get_output.R
+++ R/get_output.R
@@ -6,29 +6,29 @@ get_output <- function(model,dataframe){
   output <- predict(model, tempdf, type = "terms", se.fit = TRUE) |>
     as.data.frame()
 
-  names(output) <- str_replace_all(names(output),
+  names(output) <- stringr::str_replace_all(names(output),
                                    "^fit.s.",
                                    "smooth_")
-  names(output) <- str_replace_all(names(output),
+  names(output) <- stringr::str_replace_all(names(output),
                                    "^se.fit.s.",
                                    "smoothse_")
-  names(output) <- str_replace_all(names(output),
+  names(output) <- stringr::str_replace_all(names(output),
                                    "^fit.",
                                    "fixed_")
-  names(output) <- str_replace_all(names(output),
+  names(output) <- stringr::str_replace_all(names(output),
                                    "^se.fit.",
                                    "fixedse_")
   # Define a pattern to match strings starting with "smooth" and ending with .
   pattern <- "(smooth[^,]*)\\."
 
   # remove the . at the end of each matching string
-  names(output) <- str_replace_all(names(output), pattern, "\\1")
+  names(output) <- stringr::str_replace_all(names(output), pattern, "\\1")
 
   output <- output |>
-    select(starts_with("smooth_"),starts_with("fixed_"),starts_with("smoothse_"),starts_with("fixedse_"),everything()) |>
+    dplyr::select(starts_with("smooth_"),starts_with("fixed_"),starts_with("smoothse_"),starts_with("fixedse_"),everything()) |>
     cbind(dataframe) |>
     as.data.frame() |>
-    st_as_sf()
+    sf::st_as_sf()
 
   return(output)
 }
